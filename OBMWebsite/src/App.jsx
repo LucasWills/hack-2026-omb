d560062a89b03520e60e2a926d9f6af30df056a5
Asher Vicera <ashervicera22@gmail.com>
2026-08-07 15:30:50 -0700

    Added Logo & Name Front Screen

--- OBMWebsite/src/App.jsx
+++ OBMWebsite/src/App.jsx
@@ -1,7 +1,24 @@
+import logo from './assets/one_man_band_trnsp-2.png';
+
 export default function App() {
   return (
-    <div>
-      <h1>Welcome</h1>
+    <div className="logo-border">
+      <div className="hero-content">
+        {/* Top-Left: Logo / Graphic element */}
+        <img 
+          src={logo} 
+          alt="One Man Band Logo" 
+          className="logo-graphic"
+        />
+
+        {/* Center: The diagonal slash separator */}
+        <div className="slash-divider">/</div>
+
+        {/* Bottom-Right: Clean text layout */}
+        <div className="band-text-group">
+          <span className="band-text">ONE MAN BAND</span>
+        </div>
+      </div>
     </div>
   );
 }
